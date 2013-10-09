6319b534e0b29a649b2eed109601740cbcf84173
dawnworld <qiylei@gmail.com>
2013-10-09 09:50:00 +0800

    add objective-c and makefile

---
+++ objective-c/hello.m
@@ -0,0 +1,29 @@
+#import <Foundation/Foundation.h>
+#import <stdio.h>
+
+@interface Number: NSObject
+{
+    @public
+        int number;
+}
+
+- (void)printNum;
+@end
+
+@implementation Number: NSObject
+
+- (void)printNum
+{
+    NSLog(@"Number:%d\n", number);
+}
+
+@end
+
+int main(int argc, const char* argv[])
+{
+    printf("hello world\n");
+    Number *myNumber = [Number new];
+    myNumber->number = 6;
+    [myNumber printNum];
+    return 0;
+}
