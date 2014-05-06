b31c02d3ca133c7b6b32bfeaa588cfe15970dd2e
dawnworld <qiylei@gmail.com>
2014-05-06 10:28:53 +0800

    add new print

--- c_cpp/base/size.c
+++ c_cpp/base/size.c
@@ -13,6 +13,7 @@ int main()
     char * p_array = array;
     printf(" struct size is :%d\n", sizeof(Test_Struct));
     printf(" my var size is :%d\n", sizeof(my));
+    printf(" struct array size is %d\n", sizeof(my.a));
     printf(" array size is :%d\n", sizeof(array));
     printf(" p_array size is :%d\n", sizeof(p_array));
     printf(" long size is %d\n", sizeof(long));
