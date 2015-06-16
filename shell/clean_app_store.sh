8e4ec343aa980aa67372ced97715c8bc2374ebd3
dawnworld <thundon@qq.com>
2015-06-16 18:57:32 +0800

    remove unnecessary file with windows

--- shell/clean_app_store.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-rm -r ~/Library/Caches/com.apple.appstore
-rm -r ~/Library/Caches/com.apple.storeagent
-rm ~/Library/Preferences/com.apple.appstore.plist
-rm ~/Library/Preferences/com.apple.storeagent.plist
-rm ~/Library/Cookies/com.apple.appstore.plist
